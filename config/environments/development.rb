83bec344a3523dc9fac027edb1b4c11f6c6f4ba5
Keith Webber <keith.e.webber@gmail.com>
2014-03-31 16:49:12 -0400

    added the devise gem and ran g install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,11 @@
 CelebrityMugshots::Application.configure do
+
+
+  #DEVISE
+
+   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 CelebrityMugshots::Application.routes.draw do
+
+root to: "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
