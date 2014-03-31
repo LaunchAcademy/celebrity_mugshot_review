9febe4ceb49df1c5a646095cff062b8896c058c3
Keith Webber <keith.e.webber@gmail.com>
2014-03-31 16:07:11 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CelebrityMugshots::Application.config.secret_key_base = 'c17ba739e8f43fe32267faa21f9dd224cff26e9edfddd764b757af0c8d02fab48d2078df512bd40402c84b2bbd96420360deab9a3cc753db55a32ab945abce34'
