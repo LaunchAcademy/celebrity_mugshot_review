97dbeb603e169bff3d278f4b7be98dafdffb2d85
Dave Mullins <DaveMullinsJr@gmail.com>
2014-04-02 15:08:53 -0400

    negated uncommented lines in test file

--- spec/models/mugshot_spec.rb
+++ spec/models/mugshot_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Mugshot do
   context 'validations' do
     let (:mugshot) {Mugshot.new}
+
     it 'requres a first name' do
       expect(mugshot).to_not be_valid
       expect(mugshot.errors[:first_name]).to be_present
@@ -12,12 +13,6 @@ describe Mugshot do
       expect(mugshot).to_not be_valid
       expect(mugshot.errors[:last_name]).to be_present
     end
-
-    # it 'has an optional description' do
-    #   expect(mugshot).to be_valid
-    # end
-
-    # it 'has an optional approximate date'
 
     it 'validates added photo'
     it 'validates imaging format'
