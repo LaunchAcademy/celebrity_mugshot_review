26e4c33eb8514afd38c3c581eb8a1112bab12d5b
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-04-09 18:39:55 -0400

    fixed merge conflicts i think

--- spec/features/delete_a_mugshot_spec.rb
+++ spec/features/delete_a_mugshot_spec.rb
@@ -16,17 +16,9 @@ feature 'delete an existing mugshot', %Q{
     user = FactoryGirl.create(:user)
     sign_in_as(user)
     visit new_mugshot_path
-    fill_in 'First Name', with: 'Justin'
-    fill_in 'Last Name', with: 'Bieber'
-    fill_in 'Description', with: 'This guy should not be drunk driving'
-    select "2014" && "April" && '11', from: "Approximate Date"
-    click_button 'Create Mugshot'
 
-    expect(page).to have_content('New Mugshot Submitted!')
-    expect(page).to have_content('Bieber')
     celeb = FactoryGirl.create(:mugshot, user: user)
 
-    click_link 'Delete Mugshot'
     visit mugshot_path(celeb)
 
     click_on 'Delete'
